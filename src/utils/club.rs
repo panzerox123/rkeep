6113f8b349274c51e4dc00dc9248d31c94f7ce25
Kunal Bhat <kunal.bhat2001@gmail.com>
2021-06-07 16:39:51 +0530

    Club database : Updating and deleting clubs

--- src/utils/club.rs
+++ src/utils/club.rs
@@ -3,71 +3,95 @@ use rusqlite::{params, Connection, Result};
 #[derive(Debug)]
 
 pub struct Club {
-    id: u32,
-    club_name: String,
-    club_path: String
+    pub id: u32,
+    pub club_name: String,
+    pub club_path: String,
+}
+
+fn open_connection() -> Connection {
+    let connection = Connection::open("./testdb");
+    let connection = match connection {
+        Ok(c) => c,
+        Err(err) => panic!("Error connecting to database : {}", err),
+    };
+    return connection;
 }
 
 pub fn create_club(club_name: String, path: String) {
-    let connection = Connection::open("./testdb");
-    //let connection = Connection::open_in_memory();
-    let connection = match connection{
-        Ok(c) => c,
-        Err(err) => panic!("Error connecting to database : {}", err)
-    };
+    let connection = open_connection();
     println!("clubname: {} ; path: {}", club_name, path);
     let new_club = Club {
         id: 0,
         club_name: club_name,
-        club_path: path
+        club_path: path,
     };
     let table_create_res = connection.execute(
         "
         CREATE TABLE IF NOT EXISTS clubs (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, path TEXT NOT NULL UNIQUE);
     ", []);
-    match table_create_res{
-        Ok(_) => {},
-        Err(err) => {eprintln!("Table Creation Failed : {}", err);}
+    match table_create_res {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Table Creation Failed : {}", err);
+        }
     };
     let insert_club_res = connection.execute(
         "
         INSERT INTO clubs (name, path) VALUES(?1, ?2);
-    ", params![new_club.club_name, new_club.club_path]);
-    match insert_club_res{
-        Ok(_)=> {},
-        Err(err) => {eprintln!("Failed to create Club : {}", err);}
+    ",
+        params![new_club.club_name, new_club.club_path],
+    );
+    match insert_club_res {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Failed to create Club : {}", err);
+        }
     };
 }
 
-pub fn list_club() -> Vec<Club>{
-    let connection = Connection::open("./testdb");
-    //let connection = Connection::open_in_memory();
-    let connection = match connection{
-        Ok(c) => c,
-        Err(err) => panic!("Error connecting to database : {}", err)
-    };
+pub fn list_club() -> Vec<Club> {
+    let connection = open_connection();
     let mut statement = connection.prepare("select * from clubs").unwrap();
     let club_iterable = statement.query_map([], |row| {
-        Ok(Club{
+        Ok(Club {
             id: row.get(0)?,
             club_name: row.get(1)?,
-            club_path: row.get(2)?
+            club_path: row.get(2)?,
         })
     });
     let club_iterable = match club_iterable {
         Ok(ci) => ci,
-        Err(err) => panic!("Error loading data : {}", err)
+        Err(err) => panic!("Error loading data : {}", err),
     };
-    let mut club_list: Vec<Club> = vec!{};
-    println!("ID\tNAME\tPATH");
+    let mut club_list: Vec<Club> = vec![];
     for club in club_iterable {
         let temp = club.unwrap();
-        println!("{}\t{}\t{}", temp.id, temp.club_name, temp.club_path);
         club_list.push(temp);
     }
     return club_list;
 }
 
-fn delete_club() {}
+pub fn delete_club(clubname: String) {
+    let connection = open_connection();
+    let del_club_res = connection.execute("delete from clubs where name = ?1", params![clubname]);
+    match del_club_res {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Error deleting club : {}", err);
+        }
+    }
+}
 
-fn rename_club() {}
+pub fn update_club(clubname: String, new_clubname: String, new_path: String) {
+    let connection = open_connection();
+    let update_club_res = connection.execute(
+        "update clubs set name = ?1 , path = ?2 where name = ?3",
+        params![new_clubname, new_path, clubname],
+    );
+    match update_club_res {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("Error updating club info : {}", err);
+        }
+    }
+}
--- src/utils/cmd.rs
+++ src/utils/cmd.rs
@@ -1,10 +1,8 @@
-use super::club::create_club;
-use super::club::list_club;
+use super::club::{Club, create_club, list_club, delete_club, update_club};
 use std::env;
 
 pub fn read_args() {
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}", args);
     if args.len() == 1 {
         eprintln!("NOT IMPLEMENTED");
         return;
@@ -12,7 +10,7 @@ pub fn read_args() {
     match &args[1][..] {
         "list" => match &args[2][..] {
             "clubs" => {
-                list_club();
+                list_club_handler();
             }
             &_ => {
                 println!("Try `rkeep list help`");
@@ -23,13 +21,30 @@ pub fn read_args() {
                 let club = args[3].as_str();
                 let path = args[4].as_str();
                 create_club(club.to_string(), path.to_string());
-            }
+            },
             &_ => {
                 println!("Try `rkeep list help`");
             }
         },
+        "delete" => match &args[2][..]{
+            "club" => {
+                let club = args[3].as_str();
+                delete_club(club.to_string());   
+            },
+            &_ => {
+                println!("Try `rkeep list help`");
+            }
+        }
         &_ => {
             println!("Try `rkeep help`");
         }
+    }
+}
+
+fn list_club_handler(){
+    let data: Vec<Club> = list_club();
+    println!("ID\tNAME\tPATH");
+    for club in data {
+        println!("{}\t{}\t{}", club.id, club.club_name, club.club_path);
     }
 }
